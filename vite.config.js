a5cc59815e22455fcc17721cc8e95bfa282cf7e4
anber22 <51233546+anber22@users.noreply.github.com>
2023-06-12 19:43:25 +0800

    删除package配置文件

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 import * as path from 'path';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
-
+import alias from "@rollup/plugin-alias";
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd());
